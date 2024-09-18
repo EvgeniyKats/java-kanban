321e552801a5666afee37fcd493c9a1608a5a4c0
Evgeniy Kats <eSunr1se@yandex.ru>
2024-09-18 15:57:01 +0500

    Добавить пользовательский сценарий в Main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
-import manager.task.InMemoryTaskManager;
+import manager.Managers;
+import manager.task.TaskManager;
 import task.epic.EpicTask;
 import task.epic.SubTask;
 import task.single.SingleTask;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
+        Path path;
+        try {
+            path = Files.createTempFile("test", ".csv");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        TaskManager manager = Managers.getFileBackendTaskManager(path);
         SingleTask singleTask1 = new SingleTask("Задача1", "Описание1");
         SingleTask singleTask2 = new SingleTask("Задача2", "Описание2");
 
@@ -16,56 +27,37 @@ public class Main {
         EpicTask epicTask2 = new EpicTask("Эпик2", "ОписаниеЭпик2");
 
 //        Добавление 2 обычных задач:
-        inMemoryTaskManager.addSingleTask(singleTask1);
-        inMemoryTaskManager.addSingleTask(singleTask2);
+        manager.addSingleTask(singleTask1);
+        manager.addSingleTask(singleTask2);
 
 //        Добавление 2 эпиков:
-        inMemoryTaskManager.addEpicTask(epicTask1);
-        inMemoryTaskManager.addEpicTask(epicTask2);
+        manager.addEpicTask(epicTask1);
+        manager.addEpicTask(epicTask2);
 
 //        Добавление 3 подзадач для эпика 1:
-        EpicTask epicTaskReceived1 = inMemoryTaskManager.getEpicTask(3);
+        EpicTask epicTaskReceived1 = manager.getEpicTask(3);
         SubTask subTask1 = new SubTask("Сабтаск1", "Для эпик1", epicTaskReceived1.getId());
         SubTask subTask2 = new SubTask("Сабтаск2", "Для эпик1", epicTaskReceived1.getId());
         SubTask subTask3 = new SubTask("Сабтаск3", "Для эпик1", epicTaskReceived1.getId());
 
-        inMemoryTaskManager.addSubTask(subTask1);
-        inMemoryTaskManager.addSubTask(subTask2);
-        inMemoryTaskManager.addSubTask(subTask3);
+        manager.addSubTask(subTask1);
+        manager.addSubTask(subTask2);
+        manager.addSubTask(subTask3);
 
-        List<SingleTask> singleTasks = inMemoryTaskManager.getAllSingleTasks();
-        List<SubTask> subTasks = inMemoryTaskManager.getAllSubTasks();
-        List<EpicTask> epicTasks = inMemoryTaskManager.getAllEpicTasks();
-        System.out.println("Проверка сценария (история по порядку):");
-        for (SingleTask singleTask : singleTasks) {
-            inMemoryTaskManager.getSingleTask(singleTask.getId());
-        }
-        for (EpicTask epicTask : epicTasks) {
-            inMemoryTaskManager.getEpicTask(epicTask.getId());
-        }
-        for (SubTask subTask : subTasks) {
-            inMemoryTaskManager.getSubTask(subTask.getId());
-        }
-        System.out.println(inMemoryTaskManager.getHistory());
+        TaskManager manager2 = Managers.getFileBackendTaskManager(path);
+        List<SingleTask> singleTasks = manager.getAllSingleTasks();
+        List<SingleTask> singleTasks2 = manager2.getAllSingleTasks();
 
-        System.out.println("Проверка сценария (история в обратном порядке):");
-        for (int i = subTasks.size() - 1; i >= 0; i--) {
-            inMemoryTaskManager.getSubTask(subTasks.get(i).getId());
-        }
-        for (int i = epicTasks.size() - 1; i >= 0; i--) {
-            inMemoryTaskManager.getEpicTask(epicTasks.get(i).getId());
-        }
-        for (int i = singleTasks.size() - 1; i >= 0; i--) {
-            inMemoryTaskManager.getSingleTask(singleTasks.get(i).getId());
-        }
-        System.out.println(inMemoryTaskManager.getHistory());
+        System.out.println("SingleTasks equals=" + singleTasks.equals(singleTasks2));
 
-        System.out.println("Проверка сценария SubTask должен стать последним в истории:");
-        inMemoryTaskManager.getSubTask(subTasks.getFirst().getId());
-        System.out.println(inMemoryTaskManager.getHistory());
+        List<EpicTask> epicTasks = manager.getAllEpicTasks();
+        List<EpicTask> epicTasks2 = manager2.getAllEpicTasks();
 
-        System.out.println("Проверка сценария Epic и его сабтаски должны быть удалены из истории:");
-        inMemoryTaskManager.removeEpicTask(epicTaskReceived1.getId());
-        System.out.println(inMemoryTaskManager.getHistory());
+        System.out.println("EpicTasks equals=" + epicTasks.equals(epicTasks2));
+
+        List<SubTask> subTasks = manager.getAllSubTasks();
+        List<SubTask> subTasks2 = manager2.getAllSubTasks();
+
+        System.out.println("SubTasks equals=" + subTasks.equals(subTasks2));
     }
 }
