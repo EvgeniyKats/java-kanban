000ab8c32d1592f08907bd52d82e77f01c5e6f3b
Evgeniy Kats <eSunr1se@yandex.ru>
2024-10-16 00:41:31 +0500

    Изменить на public ManagerSaveException. Добавить коды (BaseHttpHandler)

--- src/web/handle/BaseHttpHandler.java
+++ src/web/handle/BaseHttpHandler.java
@@ -8,17 +8,40 @@ import java.nio.charset.StandardCharsets;
 public class BaseHttpHandler {
     protected static final int STATUS_SUCCESS_WITH_DATA = 200;
     protected static final int STATUS_SUCCESS_WITHOUT_DATA = 201;
+    protected static final int STATUS_BAD_REQUEST = 400;
     protected static final int STATUS_NOT_FOUND = 404;
+    protected static final int STATUS_METHOD_NOT_ALLOWED = 405;
     protected static final int STATUS_HAS_INTERACTIONS= 406;
     protected static final int STATUS_INTERNAL_SERVER_ERROR = 500;
 
     public BaseHttpHandler() {
     }
 
-    protected void sendText(HttpExchange httpExchange, String text, int statusCode) throws IOException {
+    protected void sendText(HttpExchange httpExchange, String text) throws IOException {
         byte[] resp = text.getBytes(StandardCharsets.UTF_8);
         httpExchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        httpExchange.sendResponseHeaders(statusCode, resp.length);
+        httpExchange.sendResponseHeaders(STATUS_SUCCESS_WITH_DATA, resp.length);
+        httpExchange.getResponseBody().write(resp);
+        httpExchange.close();
+    }
+
+    protected void sendSuccessWithoutBody(HttpExchange httpExchange) throws IOException {
+        httpExchange.sendResponseHeaders(STATUS_SUCCESS_WITHOUT_DATA, 0);
+        httpExchange.close();
+    }
+
+    protected void sendBadRequest(HttpExchange httpExchange, String text) throws IOException {
+        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        httpExchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        httpExchange.sendResponseHeaders(STATUS_BAD_REQUEST, resp.length);
+        httpExchange.getResponseBody().write(resp);
+        httpExchange.close();
+    }
+
+    protected void sendMethodNotAllowed(HttpExchange httpExchange, String text) throws IOException {
+        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        httpExchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        httpExchange.sendResponseHeaders(STATUS_METHOD_NOT_ALLOWED, resp.length);
         httpExchange.getResponseBody().write(resp);
         httpExchange.close();
     }
